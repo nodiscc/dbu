92dc7ddf32a3391c5ab0a62a7a82abe22ac276b5
nodiscc <nodiscc@gmail.com>
2020-09-28 18:00:17 +0200

    WIP: preconfigure thunderbird

---
+++ config/includes.chroot/etc/thunderbird/debian-live-config.js
@@ -0,0 +1,7 @@
+// Thunderbird preferences
+
+// Disable instant messaging
+user_pref("mail.chat.enabled", false);
+
+// Disable thunderbird start page
+user_pref("mailnews.start_page.enabled", false);
